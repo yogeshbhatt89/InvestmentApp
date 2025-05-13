156a3ff842d5c775612274d92b83e5f27b05a1fd
yogeshbhatt89 <61337526+yogeshbhatt89@users.noreply.github.com>
2025-05-13 13:52:26 -0400

    fixed button warning and react warnings

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 // import RegisterComponent from './features/auth/RegisterComponent'
 // import LoginComponent from './features/auth/LoginComponent'
 // import HomePage from './features/HomePage'
 // import LandingPage from './features/LandingPage';
-import DemoComponent from './features/DemoComponent'
+
 const App = () => {
-  return (
-    <Router>
-      <Routes>
-        {/* <Route path="/demo" element={<DemoComponent />} />
-        <Route path="/register" element={<RegisterComponent />} />
-        <Route path="/login" element={<LoginComponent />} />
-        <Route path="/home" element={<HomePage />} /> */}
-        <Route path="/" element={<DemoComponent />} />
-        {/* needs work */}
-      </Routes>
-    </Router>
-  )
+  return <Outlet />
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -4,19 +4,43 @@ import { ThemeProvider } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
 import { StyledEngineProvider } from '@mui/material/styles'
 import { initializeUserActivityListeners } from './utils/userActivityTracker'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import store from './app/store'
 import App from './App'
 import theme from './theme'
 import './index.css'
+import DemoComponent from './features/DemoComponent'
 
 initializeUserActivityListeners()
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '',
+        element: <DemoComponent />,
+      },
+    ],
+  },
+], {
+  future: {
+    v7_startTransition: true,
+    v7_relativeSplatPath: true,
+    v7_fetcherPersist: true,
+    v7_normalizeFormMethod: true,
+    v7_partialHydration: true,
+    v7_skipActionErrorRevalidation: true,
+  } as any // Type assertion needed until React Router v7 types are updated
+})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Provider store={store}>
     <StyledEngineProvider injectFirst>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <App />
+        <RouterProvider future={{ v7_startTransition: true }} router={router} />
       </ThemeProvider>
     </StyledEngineProvider>
   </Provider>,
