5f9158edebd6cf96b04e1d80390f88f14e09a8f6
yogeshbhatt89 <61337526+yogeshbhatt89@users.noreply.github.com>
2025-05-21 21:33:13 -0400

    removed progress percentage

--- src/modules/Table/TableWrapperComponent.tsx
+++ src/modules/Table/TableWrapperComponent.tsx
@@ -1,13 +1,16 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+
 import React from 'react'
 import Table, { TableProps } from '@mui/material/Table'
 import TableContainer from '@mui/material/TableContainer'
 import { useTable } from './useTable'
 
+export type RenderRow = (_arg: any) => React.ReactNode;
 export interface TableWrapperComponentProps extends TableProps {
   containerSx?: object
   className?: string
-  renderRow: (arg: any) => React.ReactNode;
+  /* eslint-disable @typescript-eslint/no-explicit-any */
+  renderRow: RenderRow;
   data?: any[]
 }
 
